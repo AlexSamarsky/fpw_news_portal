b5f85a25bc5edbfb4adb857fccb6b251ccc0d1bb
Самарский Алексей Сергеевич <samarskiy_a_s@sunlight.net>
2021-07-23 00:38:20 +0300

    newspaper

---
+++ NewsPaper/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+from django.db.models import Sum
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    ''' Категории
+    '''
+    name = models.CharField(max_length=150, unique=True, verbose_name='Наименование')
+
+    def __str__(self):
+        str = f'{self.name}'
+        return str
+
+
+class Author(models.Model):
+    ''' Авторы статей
+    метод обновления рейтинга - считается на основании активности автора
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Автор', primary_key=True)
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+
+    def __str__(self):
+        str = f'{self.user.username}'
+        return str
+
+    @staticmethod
+    def update_rating(user_name):
+        try:
+            user = User.objects.get(username=user_name)
+        except User.DoesNotExist as e:
+            print('unknown user')
+            return
+        
+        author = Author.objects.get(user=user)
+        post_rating = Post.objects.filter(author=author).aggregate(total=Sum('rating'))
+        user_rating = Comment.objects.filter(user=user).aggregate(total=Sum('rating'))
+        post_comments_rating = Comment.objects.filter(post__author=author).exclude(user=user).aggregate(total=Sum('rating'))
+
+        author.rating = post_rating['total'] * 3 + user_rating['total'] + post_comments_rating['total']
+        author.save()
+
+
+news = 'NW'
+article = 'ART'
+
+PostTypes = [
+    (news, 'news'),
+    (article, 'article'),
+]
+
+class Post(models.Model):
+    ''' Статья/Новость
+    '''
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Автор')
+    type = models.CharField(max_length=3, choices=PostTypes, default=article, verbose_name='Тип сообщения', null=True)
+    updated = models.DateTimeField(auto_now_add=True, verbose_name='Дата обновления')
+    title = models.CharField(max_length=250, verbose_name='Название', null=True)
+    text = models.TextField(verbose_name='Текст', null=True)
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    
+    categories = models.ManyToManyField(Category, through='PostCategory')
+    
+    def __str__(self):
+        str = f'{self.updated:%Y-%m-%d %H:%M} - author: {self.author.user.username} / title: {self.title} // {self.preview()}'
+        return str
+    
+    def like(self):
+        self.rating += 1
+        self.save()
+    
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        if len(self.text) < 124:
+            return self.text
+        else:
+            return f'{self.text[:124]}...'
+
+
+class Comment(models.Model):
+    ''' Комментарии к статьям
+    '''
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Сообщение')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    text = models.TextField(verbose_name='Текст', null=True)
+    updated = models.DateTimeField(auto_now_add=True, verbose_name='Дата обновления')
+    rating = models.IntegerField(default=0, verbose_name='Рейтинг')
+    
+    def __str__(self):
+        str = f'{self.updated:%Y-%m-%d %H:%M} - user: {self.user.username} / rating: {self.rating} // {self.text}'
+        return str
+    
+    def like(self):
+        self.rating += 1
+        self.save()
+    
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Сообщение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+
---
+++ steps.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from NewsPaper.models import Author, Category, Comment, Post, PostCategory
+user1 = User.objects.create_user('Ivanov', password='foo')
+user2 = User.objects.create_user('Petrov', password='boo')
+author1 = Author.objects.create(user=user1)
+author2 = Author.objects.create(user=user2)
+category1 = Category.objects.create(name='Sport')
+category2 = Category.objects.create(name='Economy')
+category3 = Category.objects.create(name='Astronomy')
+category4 = Category.objects.create(name='Pets')
+
+post1 = Post.objects.create(author=author1, type='ART', title='World record in dogs jumping', text='this is ever high dog jump')
+post1_category4 = PostCategory.objects.create(post=post1, category=category4)
+post1_category1 = PostCategory.objects.create(post=post1, category=category1)
+
+post2 = Post.objects.create(author=author2, type='ART', title='Black hole', text='first photo of black hole')
+post2_category3 = PostCategory.objects.create(post=post2, category=category3)
+
+news1 = Post.objects.create(author=author1, type='NW', title='Englan champion', text='England win UEFA Europe Championship')
+news1_category1 = PostCategory.objects.create(post=news1, category=category1)
+
+post1_comment1 = Comment.objects.create(user=user1, post=post1, text='comment')
+post1_comment2 = Comment.objects.create(user=user2, post=post1, text='new comment')
+post2_comment1 = Comment.objects.create(user=user1, post=post2, text='cool')
+news1_comment1 = Comment.objects.create(user=user2, post=news1, text='fantastic')
+
+post1_comment1.like()
+post1_comment2.like()
+post1_comment2.like()
+post1_comment2.like()
+post1_comment2.like()
+post1.like()
+post1.like()
+post1.like()
+post1.like()
+post2.like()
+post2.like()
+news1.like()
+post2_comment1.like()
+post1_comment2.dislike()
+
+Author.update_rating('Petrov')
+Author.update_rating('Ivanov')
+
+Author.objects.all().order_by('-rating').values('user__username', 'rating')[:1]
+
+rated_post = Post.objects.all().order_by('-rating')[0]
+print(rated_post)
+
+print(Comment.objects.filter(post=rated_post).all())
