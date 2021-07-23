db05a96225e30425e9da8a2519e6c07ac7682820
Самарский Алексей Сергеевич <samarskiy_a_s@sunlight.net>
2021-07-24 01:11:04 +0300

    templates

--- NewsPaper/views.py
+++ NewsPaper/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Post
 # Create your views here.
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    recordset = Post.objects.order_by('-id').all()
+    
+    
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
--- news_portal/settings.py
+++ news_portal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'NewsPaper',
-    'reset_migrations',
     'django_extensions',
 ]
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'news_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +65,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                'censor': 'NewsPaper.templatetags.censor'
+            }
         },
     },
 ]
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
